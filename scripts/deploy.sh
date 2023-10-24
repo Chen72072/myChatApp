903d4d926d96dc39cca74eace55f348a0e0b999d
Chen72072 <72072ch@gmail.com>
2023-10-24 09:11:09 +0300

    zipi

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,19 +2,46 @@
 
 # check the variable from the user
 if [ $# -eq 2 ]; then
-    # create a git tag
-    git tag -a $1 -m $2
-    git push --tags
+    if docker image inspect myimage:$1 ; then
+        echo "image $1 exists!!! you want to rebuild it? (y/n)"
+        read -n 1 ans
+        if [[ $ans == 'y' ]]; then
+            docker image rm myimage:$1
+            echo "Deleting existing image..."
+            # build the image
+            docker build -t myimage .
+            echo "build"
+            # create docker tag
+            docker image tag myimage myimage:$1
+            echo "tag"
+        fi
+    else
+        # image does not exist
+        echo "Image $1 does not exist. Building it now..."
 
-    # build the image
-    docker build -t myimage .
-    echo "build"
-    # create docker tag
-    docker image tag myimage myimage:$1
-    echo "tag"
-    # run the image
-    docker run -d -p 5000:5000 myimage 
-    echo "Good"
+        # build the image
+        docker build -t myimage .
+        echo "build"
+        # create docker tag
+        docker image tag myimage myimage:$1
+        echo "tag"
+    fi
+
+    echo "Do you want to push the image to the Artifact Registry repository? (y/n)"
+    read -n 1 push
+
+    if [[ $push == "y" ]]; then
+
+        # Get the service account key
+        gcloud config set auth/impersonate_service_account artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com
+
+        gcloud auth configure-docker me-west1-docker.pkg.dev
+        
+        docker tag myimage:$1 me-west1-docker.pkg.dev/grunitech-mid-project/chanicohen-chat-app-images/myimage:$1
+
+        docker push me-west1-docker.pkg.dev/grunitech-mid-project/chanicohen-chat-app-images/myimage:$1
+    fi
+
 else
     echo "ERROR - i need two argoment"
 fi
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,13 +2,31 @@
 
 version='latest'
 
-if [ $# -eq 0 ]; then
+if [ $# -ne 0 ]; then
     version=$1
+    gcloud artifacts registries Docker pull chanicohen-chat-app-images myimage:$1
+    echo "pull....."
+    docker run -d -p -v chat-app-data:/chatApp/data -p 5000:5000 --name ChatAppRun gcr.io/${gcloud config get-value project}/myimage:$version
+else
+    docker volume create chat-app-data
+    docker build -t myimage:${version} .
+    docker run -d -p -v chat-app-data:/chatApp/data -p 5000:5000 --name ChatAppRun myimage:${version}
 fi
 
-docker volume create chat-app-data
-docker build -t myimage:${version} .
-docker run -d -v chat-app-data:/chatApp/data -p 5000:5000 --name ChatAppRun myimage:${version}
+#!/bin/bash
+
+version=$1
+
+if [ $# -ne 0 ]; then
+# אם קיבלנו ארגומנט, זה אומר שאנחנו רוצים למשוך את הגרסה הספציפית הזו מהארטיפקט
+gcloud artifacts registries Docker pull chanicohen-chat-app-images myimage:versionecho"pull....."dockerrun−d−p−vchat−app−data:/chatApp/data−p5000:5000−−nameChatAppRungcr.io/{gcloud config get-value project}/myimage:$version
+else
+# אם לא קיבלנו ארגומנט, זה אומר שאנחנו רוצים לבנות את הגרסה האחרונה על המכונה הווירטואלית
+version=$(gcloud artifacts registries Docker list --filter="tags=latest" --repository=chanicohen-chat-app-images | tail -1 | awk '{print $1}')
+echo "version: version"dockervolumecreatechat−app−datadockerbuild−tmyimage:{version} .
+docker run -d -p -v chat-app-data:/chatApp/data -p 5000:5000 --name ChatAppRun gcr.io/${gcloud config get-value project}/myimage:$version
+fi
+
 
 # docker build -t myimage:${version} .
 # docker run -d -p 5000:5000 --name ChatAppRun myimage:${version}
